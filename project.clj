5e640a21272bd4a16e8cd4b40690b620eaa91ba3
Ed Porras <dev@digressed.net>
2025-08-20 21:32:43 +0200

    build: bump dependencies to latest

--- project.clj
+++ project.clj
@@ -3,19 +3,19 @@
   :url "https://github.com/edporras/nosoup-clj/"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/mit-license.php"}
-  :dependencies [[org.clojure/clojure "1.11.4"]
+  :dependencies [[org.clojure/clojure "1.12.1"]
                  [org.clojure/tools.cli "1.1.230"]
 
-                 [hiccup "1.0.5"]
+                 [hiccup "2.0.0"]
                  [ring/ring-codec "1.2.0"]
-                 [clojure.java-time/clojure.java-time "1.4.2"]
+                 [clojure.java-time/clojure.java-time "1.4.3"]
                  [sitemap "0.4.0"]
                  [org.clj-commons/digest "1.4.100"]
 
-                 [com.taoensso/timbre "6.5.0"]]
+                 [com.taoensso/timbre "6.7.1"]]
   :main ^:skip-aot nosoup-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev  {:dependencies [[babashka/fs "0.5.22"]
+             :dev  {:dependencies [[babashka/fs "0.5.26"]
                                    [hickory "0.7.1"]
                                    [org.clojure/test.check "1.1.1"]]}})
